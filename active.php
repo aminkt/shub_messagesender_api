81003ac1f422ec326de2a51f3116a08ab9cc7920
Amin <ak_1596@yahoo.com>
2017-12-30 12:35:33 +0330

    first commit

---
+++ active.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amin
+ * Date: 12/30/2017
+ * Time: 12:33 PM
+ */
---
+++ install.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @author Amin Keshvarz <amin@keshavarz.pro>
+ * @since 2017
+ */
+BOL_LanguageService::getInstance()->addPrefix('pv-api', 'وب سرویس ارسال پیام به کاربران');
